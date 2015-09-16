6b7c658ba5fd5029b3318c15eb35fe4cb6bc8894
Aleksandrs Ļedovskis <aleksandrs@ledovskis.lv>
2015-09-16 20:27:16 +0300

    Add exit-copy-past-bottom patch. Clarify description
    Signed-off-by: Aleksandrs Ļedovskis <aleksandrs@ledovskis.lv>

--- Formula/tmux-tc.rb
+++ Formula/tmux-tc.rb
@@ -1,5 +1,5 @@
 class TmuxTc < Formula
-  desc "Terminal multiplexer"
+  desc "Terminal multiplexer (with patches)"
   homepage "https://tmux.github.io/"
 
   head do
@@ -9,9 +9,16 @@ class TmuxTc < Formula
     depends_on "automake" => :build
     depends_on "libtool" => :build
 
+    # True color patch
     patch do
       url "https://gist.githubusercontent.com/zchee/9f6f2ca17acf49e04088/raw/0c9bf0d84e69cb49b5e59950dd6dde6ca265f9a1/tmux-truecolor.diff"
       sha256 "17572f1d40917a3900216b095b719c401451c93a1c61288c675a980d8031f275"
+    end
+
+    # exit-copy-past-bottom patch
+    patch do
+      url "https://github.com/dv/tmux/commit/19448c5a17f6a44ad52a2858fd40acd4b4e5aeed.patch"
+      sha256 "f8f4358cecbc99161640b7db008ef93510267c9fb927064679bd9220df8521e6"
     end
   end
 
